7fd9e1652b6319ac73f4896c7eddb98c487d5b0d
Berkenen <jberkenpas@plu.edu>
2021-03-10 12:00:45 -0800

    Added print to say that the push has to have both commits

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class hello{
 	public static void main(String[] args){
 		System.out.println("Hello");
 		System.out.println("How are you?");
+		System.out.println("Didn't do this as one push, mb");
 	} 
 }
