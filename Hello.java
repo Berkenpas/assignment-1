7ff7d5d2b6a4b7d13737a262fc12b46dc9e7c88c
Berkenen <jberkenpas@plu.edu>
2021-03-10 12:03:14 -0800

    Added print statement saying B can't push

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class hello{
 		System.out.println("Hello");
 		System.out.println("How are you?");
 		System.out.println("Didn't do this as one push, mb");
+		System.out.println("B won't be able to push hehehe")
 	} 
 }
